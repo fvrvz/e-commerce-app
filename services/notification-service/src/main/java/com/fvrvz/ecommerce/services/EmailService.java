2d934447ac99e82bbf932a5102b7f3a85cc6564c
FaraazKhhan <codefaraaz@gmail.com>
2025-02-08 19:24:48 +0530

    fixed: mailSender bean not found error

--- services/notification-service/src/main/java/com/fvrvz/ecommerce/services/EmailService.java
+++ services/notification-service/src/main/java/com/fvrvz/ecommerce/services/EmailService.java
@@ -10,8 +10,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
@@ -26,7 +26,7 @@ public class EmailService {
     private JavaMailSender _mailSender;
 
     @Autowired
-    private TemplateEngine _templateEngine;
+    private SpringTemplateEngine _templateEngine;
 
     @Async
     public void sendPaymentSuccessEmail(
