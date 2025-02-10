2dd6e4392a157bac2a10d6e42f74debd46e6b9c3
FaraazKhhan <codefaraaz@gmail.com>
2025-02-10 12:42:01 +0530

    fixed: errors for notification

--- services/product-service/src/main/java/com/fvrvz/ecommerce/controllers/ProductController.java
+++ services/product-service/src/main/java/com/fvrvz/ecommerce/controllers/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
     @Autowired
     private ProductService _productService;
